d67ed11092608f9c52aaf3f9abb2e7dfbbe0f536
tallisramon3323 <tallisramon3323@gmail.com>
2026-03-25 11:52:52 -0300

    Exercicio2

--- aula1/atividade2/atividade2.sql
+++ aula1/atividade2/atividade2.sql
@@ -8,5 +8,34 @@ foreign key (id_usuario)
 references usuario(id_usuario)
 on delete restrict;
 
-describe livro;
+alter table livro
+add column id_autor int,
+add constraint fk_livro_autor
+foreign key(id_autor)
+references autor (id_autor)
+on delete restrict;
 
+alter table livro
+add column id_editora int,
+add constraint fk_livro_editora
+foreign key (id_editora)
+references editora (id_editora)
+on delete restrict;
+
+alter table livro
+add column id_categoria int,
+add constraint fk_livro_categoria
+foreign key (id_categoria)
+references categoria (id_categoria)
+on delete restrict;
+
+
+alter table livro
+add column status_lido int,
+modify status_lido boolean default (0);
+
+alter table livro
+add constraint check_status_leitura
+check (status_lido in (0,1) );
+
+describe livro;
